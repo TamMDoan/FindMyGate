94494e8fb2d670f425f0ddacfb789d1f87bac287
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-12-20 23:59:01 -0500

    starter

---
+++ findmygate/src/main/java/com/personalproject/findmygate/FindmygateApplication.java
@@ -0,0 +1,13 @@
+package com.personalproject.findmygate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindmygateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindmygateApplication.class, args);
+	}
+
+}
---
+++ findmygate/src/test/java/com/personalproject/findmygate/FindmygateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalproject.findmygate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmygateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
