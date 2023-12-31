5d58991b1716c1e919f7ff3ddd8c0f5f7528ee28
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-12-30 23:00:54 -0500

    creating entities for db, refactoring

--- findmygate/src/main/java/com/passionproject/findmygate/entities/AdjacentGate.java
+++ findmygate/src/main/java/com/passionproject/findmygate/entities/AdjacentGate.java
@@ -1,2 +1,37 @@
-package com.passionproject.findmygate.entities;public class AdjacentGate {
+package com.passionproject.findmygate.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class AdjacentGate {
+
+    @EmbeddedId
+    PositionKey positionKey;
+
+    @ManyToOne
+    @MapsId("gateId")
+    @JoinColumn(name = "gate_id")
+    Gate gate;
+
+    @ManyToOne
+    @MapsId("adjacentId")
+    @JoinColumn(name = "adjacent_id")
+    Gate adjacentGate;
+
+    String position;
+
+    public AdjacentGate(Gate gate, Gate adjacentGate, String position) {
+        this.gate = gate;
+        this.adjacentGate = adjacentGate;
+        this.position = position;
+    }
+
+    public String getName(){
+        return adjacentGate.getName();
+    }
+
+    public Gate getGate(){
+        return adjacentGate;
+    }
+
 }
--- findmygate/src/main/java/com/passionproject/findmygate/entities/PositionKey.java
+++ findmygate/src/main/java/com/passionproject/findmygate/entities/PositionKey.java
@@ -1,6 +1,38 @@
 package com.passionproject.findmygate.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
-public class PositionKey implements Serializable {
+@Embeddable
+public class PositionKey implements Serializable{
+
+    @Column(name = "gate_id")
+    int gateId;
+
+    @Column(name = "adjacent_id")
+    int adjacentId;
+
+    public PositionKey(int gateId, int adjacentId) {
+        this.gateId = gateId;
+        this.adjacentId = adjacentId;
+    }
+
+    public int getGateId() {
+        return gateId;
+    }
+
+    public void setGateId(int gateId) {
+        this.gateId = gateId;
+    }
+
+    public int getAdjacentId() {
+        return adjacentId;
+    }
+
+    public void setAdjacentId(int adjacentId) {
+        this.adjacentId = adjacentId;
+    }
+
+
 }
